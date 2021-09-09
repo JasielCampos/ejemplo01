2d282575a654ddf6d61e5938fbf391f3add48127
Jasiel Campos Romero <jasielcamposromero@hotmail.com>
2021-09-09 02:26:09 -0400

    Datos Personales

---
+++ ejemplo01/principal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Adriana Jasiel Campos Romero" << std::endl;
+    std::cout << "Ing. de Sistemas" << std::endl;
+    std::cout << "35-5128" << std::endl;
+
+    return 0;
+}
